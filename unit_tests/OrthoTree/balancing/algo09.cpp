c33d725b2798013f06c616f6d9f7a9dbfd858124
amazzetta <aaron.mazzetta@gmail.com>
2024-12-16 18:36:39 +0100

    added unit_test file for algo9

--- unit_tests/OrthoTree/balancing/algo09.cpp
+++ unit_tests/OrthoTree/balancing/algo09.cpp
@@ -4,7 +4,7 @@
 
 using namespace ippl;
 
-TEST(CompleteSubtreeTest, TestTest) {
+TEST(RipplePropagation, TestTest) {
     static constexpr size_t Dim = 2;
     const size_t max_depth      = 6;
     const size_t max_particles  = 100;
@@ -36,7 +36,7 @@ TEST(CompleteSubtreeTest, TestTest) {
 
     Kokkos::View<morton_code*> partial_desc(descs.data(), descs.size());
 
-    auto res = tree.algo10(octant_N, partial_desc);
+    auto res = tree.algo9(partial_desc);
 
     EXPECT_EQ(res.size(), 28);
 
@@ -46,30 +46,6 @@ TEST(CompleteSubtreeTest, TestTest) {
     res[res.size() - 1] = 0;
     tree.octants_to_file(res);
     */
-}
-
-TEST(CompleteSubtreeTest, EmptyPartialDescendants) {
-    /**
-     * If we pass in an octant containing no octants that need to be rebalanced we expect the result
-     * to be empty.
-     */
-
-    static constexpr size_t Dim = 2;
-    const size_t max_depth      = 3;
-    const size_t max_particles  = 100;
-    const size_t n_particles    = 10;
-
-    BoundingBox<Dim> bounds({0.0, 0.0}, {1.0, 1.0});
-    OrthoTree<Dim> tree(max_depth, max_particles, bounds);
-    tree.setVisualisation(true);
-
-    morton_code octant_N = 1;
-    std::vector<morton_code> descs;
-    Kokkos::View<morton_code*> partial_desc(descs.data(), descs.size());
-
-    auto res = tree.algo10(octant_N, partial_desc);
-
-    EXPECT_EQ(res.size(), 0);
 }
 
 int main(int argc, char** argv) {
