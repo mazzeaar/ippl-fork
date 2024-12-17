7452436ed7809d638d9922c10b9665db9a708819
amazzetta <aaron.mazzetta@gmail.com>
2024-12-17 09:46:47 +0100

    added scaffolding for algo 11

--- src/OrthoTree/balancing/algo11.hpp
+++ src/OrthoTree/balancing/algo11.hpp
@@ -11,6 +11,35 @@ namespace ippl {
 
     template <size_t Dim>
     Kokkos::View<morton_code*> OrthoTree<Dim>::algo11(
-        Kokkos::View<morton_code*> distributed_complete_tree_L);
+        Kokkos::View<morton_code*> distributed_complete_tree_L) {
+        // B = algo4
+        // C = algo7
+        // D = stuff
+        // S = algo9
+        // F = linearise(C U S)
+        // G = stuff
+
+        for (const morton_code octant_G : std::span(G.data(), G.data() + G.size())) {
+            // algo4 octants not on this proc
+            // for each b in (B_glob - B)
+            //   if b in insulation layer (octant_G)
+            //      send g, rank(octant_G) -> step 10
+        }
+
+        // T = receive
+
+        for (const morton_code octant_G : std::span(G.data(), G.data() + G.size())) {
+            for (const morton_code octant_T : std::span(T.data(), T.data() + T.size())) {
+                // if octant_G in insulation layer of octant_T
+                //   if g was not sent to rank(octant_T) in step 10
+            }
+        }
+
+        // K = receive
+        // H = Ripple(G u T u K)
+        // R = stuff
+        // R = algo8(R)
+        // return R
+    }
 
 }  // namespace ippl
