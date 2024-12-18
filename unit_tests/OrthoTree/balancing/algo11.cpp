8bff14c651cf8ebca129dc1c8ff9665b00f4f67f
amazzetta <aaron.mazzetta@gmail.com>
2024-12-18 12:34:42 +0100

    included algo7

--- unit_tests/OrthoTree/balancing/algo11.cpp
+++ unit_tests/OrthoTree/balancing/algo11.cpp
@@ -77,8 +77,8 @@ TEST(BalancingTest, TestTest) {
     auto built_tree = tree.build_tree(particles);
     tree.setVisualisation(true);
     std::cerr << "HERE!!!!!" << std::endl;
-    auto res = tree.algo11(built_tree);
-
+    // auto res = tree.algo11(built_tree);
+    auto res = tree.algo7(0, built_tree);
     // EXPECT_EQ(res.size(), 28);
 
     // output to test
