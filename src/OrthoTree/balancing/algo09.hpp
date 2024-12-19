77c80f7bf35a13012935b25114ee3a014559ffa0
mmathieu <mmathieu@ethz.ch>
2024-12-19 02:46:47 +0100

    Algorithm 9 working, hopefully bug free and added a test for it

--- src/OrthoTree/balancing/algo09.hpp
+++ src/OrthoTree/balancing/algo09.hpp
@@ -12,6 +12,8 @@ namespace ippl {
     - UNCOMMENT THE INCLUSION OF THIS FILE IN ORTHO_TREE.HPP (BOTTOM)
     */
 
+    // THIS ALGORITHM REQUIRES SORTED INPUT
+
     // For Optimisation: If Kokkos::resize is expensive, we might want to change the implementation of the R_view
 
     template <size_t Dim>
@@ -22,13 +24,14 @@ namespace ippl {
 
         size_t R_base_size = 100;
         size_t R_index     = 0;
-        Kokkos::View<morton_code*> R_view("R_view", sorted_incomplete_tree_L.size() + R_base_size);
+        Kokkos::View<morton_code*> R_view("R_view", sorted_incomplete_tree_L.extent(0) + R_base_size);
+    
         // for l <- D_max to (L(N) + 1)
         for (size_t depth = max_depth_m; depth >= 2; depth--) {
-            Kokkos::View<std::vector<morton_code>*> T("T", W.size());
+            Kokkos::View<std::vector<morton_code>*> T("T", W.extent(0));
             // for each w in W
             std::for_each(
-                W.data(), W.data() + W.size(), [&, this](const morton_code octant) {
+                W.data(), W.data() + W.extent(0), [&, this](const morton_code octant) {
                     if (this->morton_helper.get_depth(octant) != depth) {
                         return;
                     }
@@ -38,13 +41,17 @@ namespace ippl {
                         search_keys.begin(), search_keys.end(),
                         [&, this](const morton_code current_key) {
                             auto neighbor_it =
-                                std::lower_bound(W.data(), W.data() + W.size(), current_key);
-                            if(neighbor_it == W.data() || neighbor_it == W.data()+W.size()){
+                                std::lower_bound(W.data(), W.data() + W.extent(0), current_key);
+
+                            if(neighbor_it == W.data()){
                                 return;
                             }
+
                             morton_code neighbor = *(--neighbor_it);
                             size_t neighbor_idx  = neighbor_it - W.data();
 
+                            std::cerr <<  (this->morton_helper.get_depth(neighbor) < depth - 1) << std::endl;
+                            std::cerr << (this->morton_helper.is_ancestor(current_key, neighbor)) << std::endl;
                             if (this->morton_helper.get_depth(neighbor) < depth - 1
                                 && this->morton_helper.is_ancestor(current_key, neighbor)) {
                                 T(neighbor_idx)
@@ -56,29 +63,29 @@ namespace ippl {
             auto insert_into_R = [&](morton_code octant_a, Kokkos::View<morton_code*> T_view) {
                 auto complete_subtree_view = algo10(octant_a, T_view);
 
-                const size_t additional_octants = complete_subtree_view.size();
-                size_t remaining_space          = R_view.size() - R_index;
+                const size_t additional_octants = complete_subtree_view.extent(0);
+                size_t remaining_space          = R_view.extent(0) - R_index;
 
                 while (remaining_space <= additional_octants) {
-                    Kokkos::resize(R_view, R_view.size() + R_base_size);
-                    remaining_space = R_view.size() - R_index;
+                    Kokkos::resize(R_view, R_view.extent(0) + R_base_size);
+                    remaining_space = R_view.extent(0) - R_index;
                 }
 
                 for (morton_code elem :
-                     std::span(complete_subtree_view.data(), complete_subtree_view.size())) {
+                     std::span(complete_subtree_view.data(), complete_subtree_view.extent(0))) {
                     R_view[R_index] = elem;
                     R_index++;
                 }
             };
 
-            for (size_t i = 0; i < W.size(); i++) {
+            for (size_t i = 0; i < T.extent(0); i++) {
                 if (T(i).size() != 0) {
                     Kokkos::View<morton_code*> T_view(T(i).data(), T(i).size());
                     insert_into_R(W(i), T_view);
                     T(i).clear();
                 } else {
-                    if (R_view.size() == R_index) {
-                        Kokkos::resize(R_view, R_view.size() + R_base_size);
+                    if (R_view.extent(0) == R_index) {
+                        Kokkos::resize(R_view, R_view.extent(0) + R_base_size);
                     }
 
                     R_view[R_index] = W(i);
@@ -87,6 +94,7 @@ namespace ippl {
             }
 
             Kokkos::resize(R_view, R_index);
+            std::sort(R_view.data(), R_view.data()+R_view.extent(0));
 
             R_index = 0;
             W       = R_view;
